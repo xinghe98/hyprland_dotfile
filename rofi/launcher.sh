2db8fd068b0487984c59622bf7f3e460824eb9cf
xinghe98 <lcg9899@gmail.com>
2026-02-20 22:36:21 +0800

    1

--- rofi/launcher.sh
+++ rofi/launcher.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-theme="nord"
+theme="catppuccin-frappe"
 dir="$HOME/.config/rofi/"
 
 # catppuccin
@@ -8,13 +8,6 @@ BG="#24273A"
 FG="#BF616A"
 SELECT="#1E1E2E"
 ACCENT="#24273A"
-
-# nord
-#ALPHA="#00000000"
-#BG="#3B4253"
-#FG="#BF616A"
-#SELECT="#343a46"
-#ACCENT="#3B4252"
 
 # overwrite colors file
 cat > $dir/colors.rasi <<- EOF
@@ -30,4 +23,3 @@ cat > $dir/colors.rasi <<- EOF
 EOF
 
 rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
-
